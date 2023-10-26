126cbe15ad36296d7423e652aadd8ae37a9ad419
Amit10878 <amitkumarbrya892@gmail.com>
2023-10-27 03:59:07 +0530

    changes done

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -8,9 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        int i=10;
-        int j=20;
-        int k=30;
+
+        System.out.println( "Hello World!" );
+    }
+
 }
 
 }
