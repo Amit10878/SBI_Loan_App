e8325ffa273cefcfd9018eb6583ac9960dfa3880
Amit10878 <amitkumarbrya892@gmail.com>
2023-10-28 11:03:11 +0530

    remove unwated code.

--- src/main/java/in/ashokit/App.java
+++ src/main/java/in/ashokit/App.java
@@ -9,10 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        //bug-130
-        int b =20;
-        int a =10;
-        int j=20;
+      
 
         String s = "Amit";
 
